3ea22a62fae2a857699e240eeb67de0129805e32
XIAO100he <xiao100he@gmil.com>
2019-04-30 19:31:45 +0900

    User & Group
    1. User (devise)
    2. User model, view
    3. Group model, view
    4. Group new & edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root 'messages#index'
+  devise_for :users
+  root 'groups#index'
+  resources :users
+  resources :groups do
+    resources :messages
+  end
 end
