ff250f870e924f3eed4d1b6d92b48f4218341c4a
XIAO100he <xiao100he@gmil.com>
2019-05-01 14:30:05 +0900

    Add action(routing)
    Add action(routing)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users
-  resources :groups do
-    resources :messages
+  resources :users , only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages,only: [:index]
   end
 end
