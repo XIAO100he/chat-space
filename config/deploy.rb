054e8a40c1a3bf104cc036aaa3afa6774aefee6a
XIAO100he <xiao100he@gmil.com>
2019-06-05 20:31:20 +0900

    Edit set :default_env
    ・capistranoでの自動デプロイで利用するためには、明示的に環境変数を指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,14 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+#capistranoでの自動デプロイで利用するためには、明示的に環境変数を指定
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # secrets.yml用のシンボリックリンクを追加
 #set :linked_filesで指定されたファイルは、元々のディレクトリを参照する代わりに、shared/元々のディレクトリ構成を参照するようになります。今回の例で言うと、config/secrets.ymlを参照する代わりに、shared/config/secrets.ymlを参照するようになります。
 set :linked_files, %w{ config/secrets.yml }
@@ -27,7 +35,7 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-  
+
 #desc upload secrets.yml以下の記述は、ローカル環境にあるconfig/secrets.ymlを本番環境のshared/config/secrets.ymlに反映するための設定を行なっています。これで、.gitignoreに記載されているsecrets.ymlを、Githubを経由せずにデプロイすることができます。
   desc 'upload secrets.yml'
   task :upload do
